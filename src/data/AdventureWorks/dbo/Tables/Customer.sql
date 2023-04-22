4c1013edd6c898b59e0627275b714e40a64806aa
Robert <rsmacias@gmail.com>
2023-04-22 14:43:21 -0500

    feat: add script for creating a new table - Customer.

---
+++ src/data/AdventureWorks/dbo/Tables/Customer.sql
@@ -0,0 +1,22 @@
+CREATE TABLE [dbo].[Customer]
+(
+	[CustomerID]    [int] IDENTITY(1,1) NOT NULL,
+	[NameStyle]     [bit] NOT NULL,
+	[Title]         [nvarchar](8) NULL,
+	[FirstName]     [nvarchar](50) NOT NULL,
+	[MiddleName]    [nvarchar](50) NULL,
+	[LastName]      [nvarchar](50) NOT NULL,
+	[Suffix]        [nvarchar](10) NULL,
+	[CompanyName]   [nvarchar](128) NULL,
+	[SalesPerson]   [nvarchar](256) NULL,
+	[EmailAddress]  [nvarchar](50) NULL,
+	[Phone]         [nvarchar](25) NULL,
+	[PasswordHash]  [varchar](128) NOT NULL,
+	[PasswordSalt]  [varchar](10) NOT NULL,
+	[ModifiedDate]  [datetime] NOT NULL,
+  
+ CONSTRAINT [PK_Customer_CustomerID] PRIMARY KEY CLUSTERED 
+  (
+	  [CustomerID] ASC
+  ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+)
