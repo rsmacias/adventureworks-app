860b56162fc640655d23cc1aa917bd54c318c0ea
Robert <rsmacias@gmail.com>
2023-04-29 23:26:45 -0500

    feat: add entity for ProductCategory table.

---
+++ src/AdventureWorks.data.domain/ProductCategory.cs
@@ -0,0 +1,14 @@
+namespace AdventureWorks.Data.Domain;
+
+public class ProductCategory {
+    public int ProductCategoryId { get; set; }
+
+    public int? ParentProductCategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateTime ModifiedDate { get; set; }  
+
+    public ProductCategory? ParentProductCategory { get; set; }  
+    
+}
